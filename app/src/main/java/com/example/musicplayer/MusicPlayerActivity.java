1cd8bd3bb64403ea100b1eb8381d5db58d8e9845
Kopelec Martin <kop0213@vsb.cz>
2019-12-13 18:01:32 +0100

    Vyřešení bugů - správnost funkce zobrazování trvání písně.

--- app/src/main/java/com/example/musicplayer/MusicPlayerActivity.java
+++ app/src/main/java/com/example/musicplayer/MusicPlayerActivity.java
@@ -1,15 +1,28 @@
 package com.example.musicplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.res.ResourcesCompat;
 
 import android.content.Intent;
+import android.graphics.BlendModeColorFilter;
+import android.graphics.ColorFilter;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.PorterDuffXfermode;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.io.File;
 import java.util.ArrayList;
@@ -22,13 +35,34 @@ public class MusicPlayerActivity extends AppCompatActivity {
 
     static MediaPlayer mediaPlayer;
     int pos;
+    //int colorPrimary = R.color.colorPrimary;
     String songName, actualSongName;
 
     ArrayList<File> songList;
     Thread updateDuration; // Vyuziti threadu pri praci s zobrazovanim prubehu pisne
 
+    /*
+    Tvorba menu v aktivite prehravace
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.player_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_music_player);
 
@@ -39,6 +73,12 @@ public class MusicPlayerActivity extends AppCompatActivity {
         pauseButton = findViewById(R.id.pauseButton);
 
         songDurationBar = findViewById(R.id.durationBar);
+
+
+
+        getSupportActionBar().setTitle("Now playing");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
 
         updateDuration = new Thread(){ // Vyuziti threadu pri praci s zobrazovanim prubehu pisne
             @Override
@@ -53,7 +93,7 @@ public class MusicPlayerActivity extends AppCompatActivity {
                  */
                 while(currentDuration<totalDuration){
                     try{
-                        sleep(500);
+                        sleep(1500);
                         currentDuration = mediaPlayer.getCurrentPosition();
                         songDurationBar.setProgress(currentDuration);
                     }
@@ -83,6 +123,15 @@ public class MusicPlayerActivity extends AppCompatActivity {
 
         mediaPlayer.start();
         songDurationBar.setMax(mediaPlayer.getDuration());
+
+        updateDuration.start(); // volam update na duration, startuju thread
+        //songDurationBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.MULTIPLY);
+        songDurationBar.getProgressDrawable().setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary), PorterDuff.Mode.MULTIPLY));
+
+        songDurationBar.getProgressDrawable().setColorFilter(new PorterDuffColorFilter(ContextCompat.getColor(getApplicationContext(), R.color.colorPrimary), PorterDuff.Mode.SRC_IN));
+        //songDurationBar.getProgressDrawable().setColorFilter(new PorterDuffColorFilter(R.color.colorPrimary, PorterDuff.Mode.SRC_IN));
+        //songDurationBar.getProgressDrawable().setColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC_IN);
+
 
         songDurationBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
@@ -135,6 +184,7 @@ public class MusicPlayerActivity extends AppCompatActivity {
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
                 songName = songList.get(pos).getName();
                 songNameTextView.setText(songName);
+                songDurationBar.setMax(mediaPlayer.getDuration());
 
                 mediaPlayer.start();
             }
@@ -147,17 +197,21 @@ public class MusicPlayerActivity extends AppCompatActivity {
         previousButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
                 mediaPlayer.stop();
                 mediaPlayer.release();
-                pos = ((pos-1)%songList.size()); // zde se urcuje nova pozice
+                pos = ((pos-1)<0)?(songList.size()-1):(pos-1); // zde se urcuje nova pozice
 
                 Uri uri = Uri.parse(songList.get(pos).toString());
                 mediaPlayer = MediaPlayer.create(getApplicationContext(), uri);
                 songName = songList.get(pos).getName();
                 songNameTextView.setText(songName);
+                songDurationBar.setMax(mediaPlayer.getDuration());
 
                 mediaPlayer.start();
             }
         });
     }
+
+
 }
